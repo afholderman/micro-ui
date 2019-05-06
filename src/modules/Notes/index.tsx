cfe6c8229456f55f4db0dcce58fd95504a16aba9
afholderman <aaron@pieinsurance.com>
2019-05-06 10:57:23 -0600

    remove unused imports

--- src/modules/Notes/index.tsx
+++ src/modules/Notes/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Col } from "antd";
 import { NoteList } from "./components/NoteList";
 import { AddNote } from "./components/AddNote";
-import { ReducerRegistry } from "../../data/reducerRegistry";
 
 export const NoteModule: React.FC = () => (
   <Col sm={{ span: 22 }} md={{ span: 18 }} lg={{ span: 12 }}>
--- src/modules/ToDo/index.tsx
+++ src/modules/ToDo/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Col } from "antd";
 import { ToDoList } from "./components/ToDoList";
 import { AddToDo } from "./components/AddToDo";
-import { ReducerRegistry } from "../../data/reducerRegistry";
 
 export const ToDoModule: React.FC = () => (
   <Col sm={{ span: 22 }} md={{ span: 18 }} lg={{ span: 12 }}>
