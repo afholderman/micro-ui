58d0cd123a847e6b685a7f798f6407f0fa7e7619
afholderman <aaron@pieinsurance.com>
2019-05-06 10:56:11 -0600

    remove unused interfaces

--- src/modules/Notes/index.tsx
+++ src/modules/Notes/index.tsx
@@ -4,10 +4,6 @@ import { NoteList } from "./components/NoteList";
 import { AddNote } from "./components/AddNote";
 import { ReducerRegistry } from "../../data/reducerRegistry";
 
-interface NoteModuleProps {
-  reducerRegistry: ReducerRegistry;
-}
-
 export const NoteModule: React.FC = () => (
   <Col sm={{ span: 22 }} md={{ span: 18 }} lg={{ span: 12 }}>
     <NoteList />
--- src/modules/ToDo/index.tsx
+++ src/modules/ToDo/index.tsx
@@ -4,10 +4,6 @@ import { ToDoList } from "./components/ToDoList";
 import { AddToDo } from "./components/AddToDo";
 import { ReducerRegistry } from "../../data/reducerRegistry";
 
-interface ToDoModuleProps {
-  reducerRegistry: ReducerRegistry;
-}
-
 export const ToDoModule: React.FC = () => (
   <Col sm={{ span: 22 }} md={{ span: 18 }} lg={{ span: 12 }}>
     <ToDoList />
